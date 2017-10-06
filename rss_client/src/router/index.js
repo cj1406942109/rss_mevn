6b487188ba857d72ce9eb0b1d83518d2b7a2df57
cj1406942109 <1406942109@qq.com>
2017-10-06 21:06:21 +0800

    添加拦截功能

--- rss_client/src/router/index.js
+++ rss_client/src/router/index.js
@@ -6,7 +6,7 @@ import home from '@/components/home/home'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
     routes: [{
         path: '/',
         redirect: '/login'
@@ -17,10 +17,33 @@ export default new Router({
     }, {
         path: '/home',
         name: 'home',
+        meta: {
+            requireAuth: true
+        },
         component: home
     }, {
         path: '/register',
         name: 'register',
         component: register
-    }]
+    }],
+    beforeEach: (to, from, next) => {
+        console.log('hello');
+
+    }
 })
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requireAuth) { // 判断该路由是否需要登录权限
+        if(sessionStorage.getItem('user')) { // 通过vuex state获取当前的token是否存在
+            next();
+        } else {
+            next({
+                path: '/login',
+                query: { redirect: to.fullPath } // 将跳转的路由path作为参数，登录成功后跳转到该路由
+            })
+        }
+    } else {
+        next();
+    }
+})
+export default router;
